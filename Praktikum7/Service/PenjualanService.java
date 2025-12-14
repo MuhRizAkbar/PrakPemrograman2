efaef7b889039a6cd304a906f4d35385479c00a9
MuhRizAkbar <2410817210016@mhs.ulm.ac.id>
2025-12-15 02:28:35 +0800

    Update PenjualanService.java

--- Praktikum7/Service/PenjualanService.java
+++ Praktikum7/Service/PenjualanService.java
@@ -15,28 +15,25 @@ public class PenjualanService {
     }
 
     public void addData(Penjualan p) throws Exception {
-        // 1. Ambil data buku berdasarkan ID
+
         Buku buku = bukuDao.findById(p.getBukuId());
         
-        // 2. Cek apakah buku ada
         if (buku == null) {
             throw new Exception("Buku tidak ditemukan!");
         }
 
-        // 3. Cek stok cukup atau tidak
         if (buku.getStok() < p.getJumlah()) {
             throw new Exception("Stok buku tidak mencukupi! Sisa: " + buku.getStok());
         }
-
-        // 4. Kurangi stok buku
+        
         buku.setStok(buku.getStok() - p.getJumlah());
-        bukuDao.update(buku); // Update stok di database
+        bukuDao.update(buku); 
 
-        // 5. Simpan transaksi penjualan
         dao.save(p);
     }
 
     public void deleteData(int id) {
         dao.delete(id);
     }
+
 }
