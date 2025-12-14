5f7465516278a7dda851f3fe66c275b12b07774f
MuhRizAkbar <2410817210016@mhs.ulm.ac.id>
2025-12-15 02:27:58 +0800

    Update PelangganService.java

--- Praktikum7/Service/PelangganService.java
+++ Praktikum7/Service/PelangganService.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public class PelangganService {
     private final PelangganDaoImpl dao = new PelangganDaoImpl();
     
-    // Regex simple untuk cek format email dan angka
+
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
     private static final Pattern PHONE_PATTERN = Pattern.compile("^[0-9]+$");
 
@@ -42,4 +42,5 @@ public class PelangganService {
             throw new Exception("Telepon harus berupa angka!");
         }
     }
+
 }
